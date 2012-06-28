87e6aafe5d6100aa14e4f0763540ef09dd8f5882
tokuhirom <tokuhirom@gmail.com>
2012-06-28 12:42:14 +0900

    initial import

---
+++ lib/HTTP/Session/Store/DBI/Lite.pm
@@ -0,0 +1,39 @@
+package HTTP::Session::Store::DBI::Lite;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+HTTP::Session::Store::DBI::Lite - A module for you
+
+=head1 SYNOPSIS
+
+  use HTTP::Session::Store::DBI::Lite;
+
+=head1 DESCRIPTION
+
+HTTP::Session::Store::DBI::Lite is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
